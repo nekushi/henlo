860c817e7b0be319868ec61f2e10aca524577df2
nekushi <122607874+nekushi@users.noreply.github.com>
2024-03-16 15:32:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 const mainBox = document.querySelector("#main-box")
 const lidBox = document.querySelector(".box-lid")
+const divEmoji = document.querySelectorAll("div.emoji")
+
+const h2 = document.createElement("h2")
+mainBox.appendChild(h2)
 
 function throwLid() {
     lidBox.style.top = "40%"
@@ -10,14 +14,30 @@ function throwLid() {
 function lowerMainBox() {
     mainBox.style.top = "90%"
 }
-const p = document.createElement("p")
 function doMagic() {
-    p.innerText = "Hello world"
-    mainBox.appendChild(p)
-
-    p.style.animation = "fireworks 1000ms ease-in-out forwards"
+    h2.innerText = "Hello world"
+    h2.style.animation = "popUp 1000ms ease-in-out forwards"
 }
+function fireworks() {
+    let count = setInterval(() => {
+        const emojis = ["🥳", "💜", "🩷", "🎉", "🎈", "🎊", "🎁"]
+        let docWidth = window.innerWidth - 35
+        let docHeight = window.innerHeight - 72
 
+        const createEmoji = document.createElement("div")
+        createEmoji.classList.add("emoji")
+        let randNum = Math.floor(Math.random() * emojis.length)
+        createEmoji.innerText = `${emojis[randNum]}`
+        createEmoji.style.top = `${Math.floor(Math.random() * docHeight)}px`
+        createEmoji.style.left = `${Math.floor(Math.random() * docWidth)}px`
+        createEmoji.style.transform = `scale(${(Math.random() * 2) + 1})`
+        document.body.appendChild(createEmoji)
+        createEmoji.style.animation = "fadeDrop 750ms linear forwards"
+    }, 10);
+    setTimeout(() => {
+        clearInterval(count)
+    }, 1000)
+}
 
 
 function lidVanish() {
@@ -38,7 +58,41 @@ function speakNow() {
     return new Promise(function(resolve, reject) {
         setTimeout(() => {
             resolve(doMagic())
-        }, 3);
+        }, 200);
+    })
+}
+function throwEmojis() {
+    return new Promise((resolve, reject) => {
+        resolve(fireworks())
+    //     let count = setInterval(() => {
+    //         const emojis = ["🥳", "💜", "🩷", "🎉", "🎈", "🎊", "🎁"]
+    //         let docWidth = window.innerWidth - 32
+    //         let docHeight = window.innerHeight - 32
+    
+    //         const createEmoji = document.createElement("div")
+    //         createEmoji.classList.add("emoji")
+    //         let randNum = Math.floor(Math.random() * emojis.length)
+    //         createEmoji.innerText = `${emojis[randNum]}`
+    //         createEmoji.style.top = `${Math.floor(Math.random() * docHeight)}px`
+    //         createEmoji.style.left = `${Math.floor(Math.random() * docWidth)}px`
+    //         createEmoji.style.transform = `scale(${(Math.random() * 1) + 1})`
+    //         document.body.appendChild(createEmoji)
+    //     }, 20);
+    //     setTimeout(() => {
+    //         // for (let i = 0; i < divEmoji.length; ++i) {
+    //         //     divEmoji[i].style.transform = "translateY(50px)"
+    //         //     divEmoji[i].style.animation = "fadeDrop 300ms linear forwards"
+    //         // }
+    //         // divEmoji.forEach(x => {
+    //         //     x.style.transform = "translateY(50px)"
+    //         //     x.style.animation = "fadeDrop 300ms linear forwards"
+    //         // })
+    //         clearInterval(count)
+    //     }, 1000)
+    //     divEmoji.forEach(x => {
+    //         x.style.transform = "translateY(50px)"
+    //         x.style.animation = "fadeDrop 300ms linear forwards"
+    //     })
     })
 }
 
@@ -48,6 +102,7 @@ async function doAllShit() {
         const first = await lidVanish()
         const second = await lowerMainBox()
         const third = await speakNow()
+        const fourth = await throwEmojis()
     } catch (error) {
         alert(error)
     }
